506ac4c46130829de09407ad6aa4382001766a5f
Svetlana <sveta-dekt@tut.by>
2021-08-15 15:38:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+  let arr = String(n).split('');
   
-}
+  let newArr = arr.reverse();
+  
+  for(let i = 0; i < newArr.length; i++) {
+      if (newArr[i] == '-') {
+          newArr.pop();
+      }
+  }
+  let reversed = +(newArr.join(''));
+  return reversed ;
+};
+
+
+
+
